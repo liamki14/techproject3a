be7a15c44691e9a3804abde03f8775087b891cf2
liamki14 <70179495+liamki14@users.noreply.github.com>
2020-09-24 20:30:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Author:Liam Kelly
+//Date: 9/24/2020
+//Description: Tech Project 3a. Methods
+
+using System;
+
+namespace techproject3a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter your first and last name");
+            string input = Console.ReadLine();
+            
+            printName(input);
+        } 
+              
+        private static void printName(string name)
+        {
+            
+            if (name == "Liam Kelly")
+                Console.WriteLine("Hello " + name + " - Go Bolts!");            
+            else
+                Console.WriteLine("Hello " + name + " - Have a great day");
+
+        }
+    }
+}
